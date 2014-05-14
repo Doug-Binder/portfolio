b72f7138fa8e3fe036e44b1f3cb8a0e3974c60d6
Doug-Binder <binderd@stolaf.edu>
2014-05-14 10:58:07 -0500

    data is being pushed into mongo from NYT

--- node/app.js
+++ node/app.js
@@ -5,7 +5,7 @@ var express = require('express');
 var app = express();
 var http = require('http');
 var request = require("request");
-
+var MongoClient = require('mongodb').MongoClient;
 
 
 var congressNumber = '113';
@@ -16,6 +16,7 @@ var apiKey = '09373818ca6661316b7b4fc325c569fd:6:69165979';
 var url = 'http://api.nytimes.com/svc/politics/'+ version + '/us/legislative/congress/'+ congressNumber + '/' + chamber + '/' +
 'members.json?api-key='+apiKey;
 
+var data;
 
 
 request({
@@ -24,15 +25,22 @@ request({
 	},
 		function(error, response, body){
  			if (!error && response.statusCode === 200) {
-        	//	console.log(body['results'][0]['members']) // Print the json response
-  	}
-  })
+        		data = body['results'][0]['members']; // Print the json response
+        		MongoClient.connect("mongodb://localhost:27017/exampleDb", function(err, db) {
+				  if(!err) {
+				    console.log("We are connected");
+				     var collection = db.collection('test');
+				     collection.insert(data, {w:1}, function(err, result) {
+				     	if(!err){console.log('congress data inserted')}
+				     });
 
+				  }
+				});
+        	}
+  		})
+ 
 
-app.get('/idea', function (req,res){
-
-
-});
-console.log(url);
+app.get('/', function (req,res){});
+//console.log(url);
 
 app.listen(3000);
